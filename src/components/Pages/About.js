def7ee53749990ca8d285d659e6b50c2f0e249b4
lakshyavijh <lakshya.vijh99@gmail.com>
2020-03-26 14:54:51 +0530

    first commit

---
+++ src/components/Pages/About.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react'
+
+ const About = () => {
+    return (
+        <Fragment>
+            <h1>About this App</h1>
+            <div>
+            <h2>Simple app to search github users</h2>
+            <h2> Developed by Lakshya Vijh</h2>
+            </div>
+            
+        </Fragment>
+    )
+}
+export default About
